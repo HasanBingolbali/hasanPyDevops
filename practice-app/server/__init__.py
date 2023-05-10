a3c0354e5b4185a60f29408e71011450b8d179df
HasanBingölbali <74209102+HasanBingolbali@users.noreply.github.com>
2023-05-10 15:11:24 +0300

    added missing requirements

---
+++ practice-app/server/__init__.py
@@ -0,0 +1,28 @@
+def home():
+     try:
+         conn = psycopg2.connect(
+             host="172.18.0.2",
+             database="postgres",
+             user="postgres",
+             password="postgres",
+             port="5432"
+         )
+         cur = conn.cursor()
+
+         # execute a query
+         cur.execute("SELECT * FROM accounts")
+
+         hasan = ""
+         # print the results
+         for row in cur:
+             for i in row:
+                 osman += str(i)
+
+         cur.close()
+         conn.close()
+
+         print(hasan)
+         return {"message": hasan}
+     except Exception as e:
+         return {"error": str(e)}
+         
